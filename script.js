8ac7211cd8124a70dba5551530eaccdb33da6d65
Ivan Revyakin <LovingFox@GMail.com>
2020-06-15 10:15:02 +0300

    chamge url

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const url = "https://api.myjson.com/bins/jcmhn";
+const url = "https://api.jsonbin.io/b/5e905926172eb643896166e7";
 const vars=["var1", "var2", "var3", "var4", "var5", "var6", "speach"];
 
 function hButton(e) {
